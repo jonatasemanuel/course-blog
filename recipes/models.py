d79c1f457a5113428dbd97ba89e27a26a136cba4
Jonatas Silva <jonatassilvaemanuel@gmail.com>
2023-02-11 17:12:02 -0300

    css and media

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import User
+
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField(null=True)
+    preparation_time = models.IntegerField()
+    preparation_unit = models.CharField(max_length=65)
+    servigs = models.IntegerField()
+    servigs_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField()
+    preparation_step_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
+    
+    def __str__(self) -> str:
+        return self.title
