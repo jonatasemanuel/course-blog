e47cd6077a6509d343d4b1b7197f947c7e2e6167
Jonatas E S Silva <jonatassilvaemanuel@gmail.com>
2023-02-13 23:25:14 -0300

    query set and fixed issues

--- recipes/views.py
+++ recipes/views.py
@@ -1,27 +1,44 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_list_or_404, render
+from django.http import Http404, HttpResponse
 
 from utils.recipes.factory import make_recipe
 
-from .models import Recipe, Category
+from .models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     context = {'recipes': recipes}
     return render(request, 'recipes/pages/home.html', context)
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id
+
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True
         ).order_by('-id')
-    context = {'recipes': recipes,}
-    return render(request, 'recipes/pages/home.html', context)
+    )
+
+    context = {'recipes': recipes,
+                'title': f'{recipes[0].category.name}'
+    }
+    return render(request, 'recipes/pages/category.html', context)
 
 
 def recipe(request, id):
-    context = {'recipe': make_recipe(),
+    
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True,
+    ).order_by('-id').first()
+    
+    context = {'recipe': recipe,
                'is_detail_page': True,
-            }
+    }
+    
     return render(request, 'recipes/pages/recipe-view.html', context)
