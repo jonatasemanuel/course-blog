e8b8a57e67685e1512d5210fcda0b1dbcd0f3372
Jonatas E S Silva <jonatassilvaemanuel@gmail.com>
2023-02-15 23:34:55 -0300

    implementation: tests

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.http import Http404, HttpResponse
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from utils.recipes.factory import make_recipe
 
@@ -25,22 +25,22 @@ def category(request, category_id):
     )
 
     context = {'recipes': recipes,
-                'title': f'{recipes[0].category.name}'
-    }
+               'title': f'{recipes[0].category.name}'
+               }
     return render(request, 'recipes/pages/category.html', context)
 
 
 def recipe(request, id):
- 
+
     # recipe = Recipe.objects.filter(
     #     pk=id,
     #     is_published=True,
     # ).order_by('-id').first()
 
-    recipe =get_object_or_404(Recipe, pk=id, is_published=True)
-    
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
+
     context = {'recipe': recipe,
                'is_detail_page': True,
-    }
-    
+               }
+
     return render(request, 'recipes/pages/recipe-view.html', context)
